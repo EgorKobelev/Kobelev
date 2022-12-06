9219a1355afd9214bb3b1ad48a4b4c5224d6a2df
AricsyGIT <aricsybsn@gmail.com>
2022-12-07 04:11:32 +0500

    Улучшил восприятие кода

--- entry_point.py
+++ entry_point.py
@@ -1,8 +1,8 @@
 import pretty_table_generator
 import pdf_generator
 
-key = input("Напишите 1, если хотите сгенерировать таблицу. Напишите 2, если хотите сформировать pdf ")
-if (key == "1"):
+user_choice = input("Напишите 1, если хотите сгенерировать таблицу. Напишите 2, если хотите сформировать pdf ")
+if (user_choice == "1"):
     pretty_table_generator.get_pretty_table()
-elif (key == "2"):
+elif (user_choice == "2"):
     pdf_generator.get_pdf_statistic()
